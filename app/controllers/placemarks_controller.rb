c478a9c4754433f38b2a283d9a6fa9c6c184a55e
Nick <nbumbarger@gmail.com>
2015-05-11 12:26:19 -0400

    Add basic controllers, add introductory text.

---
+++ app/controllers/placemarks_controller.rb
@@ -0,0 +1,37 @@
+class PlacemarksController < ApplicationController
+
+  def index
+    @placemarks = Placemark.all
+  end
+
+  def show
+    @placemark = Placemark.find(:id)
+  end
+
+  def new
+    @placemark = Placemark.new
+  end
+
+ def create
+    @story = Story.find(params[:story_id])
+    @placemark = @story.placemarks.new(placemark_params)
+    if @placemark.save
+      redirect_to @story
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:story_id])
+    @placemark = @story.placemarks.find(params[:id])
+    @placemark.destroy
+    redirect_to @story
+  endd
+
+  private
+  def placemark_params
+    params.require(:placemark).permit(:name, :synopsis, :full_text, :lat, :lon)
+  end
+
+end
---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,35 @@
+class StoriesController < ApplicationController
+
+  def index
+    @stories = Story.all
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+      if @story.save
+        redirect_to @story
+      else
+        render :new
+      end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    redirect_to stories_path
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:name)
+  end
+
+end
