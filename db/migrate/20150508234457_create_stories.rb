87e07ffc61f0ea4233bcac4c4966a3bd63defdb6
Nick <nbumbarger@gmail.com>
2015-05-17 14:52:36 -0400

    Added style to placemark popup baloons; altered image aspect ratio

--- db/migrate/20150508234457_create_stories.rb
+++ db/migrate/20150508234457_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
       t.string :name, :null=> false
-      t.string :description
+      t.text :description
       t.datetime :deleted_at
     end
   end
--- db/migrate/20150508234521_create_placemarks.rb
+++ db/migrate/20150508234521_create_placemarks.rb
@@ -6,8 +6,8 @@ class CreatePlacemarks < ActiveRecord::Migration
       t.text :description
       t.decimal :lat, :null=> false
       t.decimal :lng, :null=> false
-      t.binary :image_data, :null=> false
-      t.string :image_alt, :null=> false
+      t.binary :image_data
+      t.string :image_alt
       t.datetime :deleted_at
     end
   end
