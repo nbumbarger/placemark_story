38a89c165bd9c9f92f8c5406acc8fa2223efbad2
Nick Bumbarger <nbumbarger@gmail.com>
2015-06-27 17:57:35 -0400

    Test Heroku settings

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,9 +7,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-
-config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.svg] 
-
-
 Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.svg *.eot *.ttf *.woff]
 Rails.application.config.assets.precompile += %w[html5shiv.js html5shiv-printshiv.js]
