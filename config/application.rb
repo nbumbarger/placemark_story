f6833cc1a324438377fa149d5d1fb2d3e0c991c5
Nick Bumbarger <nbumbarger@gmail.com>
2015-06-27 18:43:37 -0400

    Testing Heroku settings

--- config/application.rb
+++ config/application.rb
@@ -2,19 +2,19 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Bundler group settings
-bundler_settings = { 
-  basic_debug:          %w(development test),
-  quality_analysis:     %w(development test),
-  performance_analysis: %w(development test),
-  documentation:        %w(development test),
-  heroku:               %w(production)
-}
-# Require general functions for all application environments, map other
-# function groups to application environments as indicated in bundler_settings
-if defined?(Bundler)
-  Bundler.require(*Rails.groups(bundler_settings))
-end
+# # Bundler group settings
+# bundler_settings = { 
+#   basic_debug:          %w(development test),
+#   quality_analysis:     %w(development test),
+#   performance_analysis: %w(development test),
+#   documentation:        %w(development test),
+#   heroku:               %w(production)
+# }
+# # Require general functions for all application environments, map other
+# # function groups to application environments as indicated in bundler_settings
+# if defined?(Bundler)
+#   Bundler.require(*Rails.groups(bundler_settings))
+# end
 
 module PlacemarkStory
   class Application < Rails::Application
